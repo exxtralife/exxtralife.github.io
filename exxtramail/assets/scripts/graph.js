5f7905445b20ec91d11027604063b2a9ef083311
edwxn-13 <73799683+edwxn-13@users.noreply.github.com>
2025-02-07 03:54:04 +0000

    liteupdate

--- exxtramail/assets/scripts/graph.js
+++ exxtramail/assets/scripts/graph.js
@@ -1,3 +1,32 @@
+class Line extends PIXI.Graphics {
+    constructor(points, lineSize, lineColor) {
+        super();
+        
+        var s = this.lineWidth = lineSize || 5;
+        var c = this.lineColor = lineColor || "0x000000";
+        
+        this.points = points;
+
+        this.lineStyle(s, c)
+
+        this.moveTo(points[0], points[1]);
+        this.lineTo(points[2], points[3]);
+    }
+    
+    updatePoints(p) {
+        
+        var points = this.points = p.map((val, index) => val || this.points[index]);
+        
+        var s = this.lineWidth, c = this.lineColor;
+        
+        this.clear();
+        this.lineStyle(s, c);
+        this.moveTo(points[0], points[1]);
+        this.lineTo(points[2], points[3]);
+    }
+}
+
+
 (async ()=>
 {
     var client = document.getElementById("page");
@@ -6,9 +35,58 @@
     var h = window.innerHeight;
 
     const app = new PIXI.Application();
-    await app.init({ background: '#EEEEEE', width: w * 0.989, height: h * 0.982 });
+    await app.init({ background: '#FFFFFF', width: w * 0.989, height: h * 0.982 });
     
     document.getElementById("page").appendChild(app.canvas);
+
+    const hey_image = await PIXI.Assets.load('assets/images/band.png');
+
+    /*
+    var particle = new PIXI.Sprite(hey_image);
+  
+
+    particle.x = app.screen.width / 2;
+    particle.y = app.screen.height / 2;
+    particle.anchor.set(0.5);
+
+    app.stage.addChild(particle);
+    */
+
+
+    const thing = new PIXI.Graphics();
+    app.stage.addChild(thing);
+    thing.x = app.screen.width / 2;
+    thing.y = app.screen.height / 2;
+
+
+
+    var particle_list = [];
+
+    const circle = await PIXI.Assets.load('assets/images/circle.png');
+    
+    var ubi_particle = new PIXI.Sprite(circle);
+    particle_list.push(ubi_particle);
+    var fble_particle = new PIXI.Sprite(circle);
+    particle_list.push(fble_particle);
+
+    var mk_particle = new PIXI.Sprite(circle);
+    particle_list.push(mk_particle);
+
+    var ash_particle = new PIXI.Sprite(circle);
+    particle_list.push(ash_particle);
+
+    var trshgrrrl = new PIXI.Sprite(circle);
+    particle_list.push(trshgrrrl);
+
+    for (let index = 0; index < particle_list.length; index++) {
+        particle_list[index].anchor.set(0.01);
+        particle_list[index].scale = 0.05;
+        app.stage.addChild(particle_list[index]);
+    }
+
+    var line = new Line([200, 150, 0, 0]);
+    app.stage.addChild(line);
+
 
     //mouse trail stuff 
 
@@ -18,7 +96,6 @@
     const historySize = 5;
     const ropeSize = 200;
     const points = [];
-
 
     for (let i = 0; i < historySize; i++)
     {
@@ -48,8 +125,20 @@
     });
     //end of mouse trail stuff
 
+    var whole_time = 0.0;
+
     app.ticker.add((time) =>
     {
+
+
+        whole_time += time.deltaTime;
+        //particle.rotation = Math.sin(0.01 * whole_time);
+
+        for (let index = 0; index < particle_list.length; index++) {
+            particle_list[index].x =  (app.screen.width * 0.5) + app.screen.width * 0.4 * Math.sin(0.01 * (index+1/8) * whole_time + (15 * index));
+            particle_list[index].y = (app.screen.height * 0.75) + app.screen.height * 0.1 * Math.sin(0.02 * whole_time + (15 * index));
+        }
+            
 
         //mouse trail stuff
         if (!mouseposition) return;
@@ -70,6 +159,11 @@
             p.x = ix;
             p.y = iy;
         }
+
+        thing.clear();
+        thing.moveTo(particle_list[1].x, particle_list[1].y)
+        thing.lineTo(particle_list[0].x, particle_list[0].y)
+        thing.stroke({ width: 0.8, color: 0x000000 });
     });
 
 
