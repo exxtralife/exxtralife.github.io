7599b2e6ec3f58be5b5cb089aa84cfcc92275d05
psyje6 <psyje6@nottingham.ac.uk>
2025-06-27 23:19:34 +0100

    Adding font and bottom panel is started

--- src/components/textPanel.jsx
+++ src/components/textPanel.jsx
@@ -1,14 +1,11 @@
-
-
-
 const TextPanel = () => 
-    {
-        return
-        (
-            <div>
-                <h1>ARGHHHHHHHHHHH</h1>
-            </div>
-        )
-    }
-
+{
+    return (
+        <>
+        <div>      
+            ARGHHHHH
+        </div>
+        </>
+    )
+}
 export default TextPanel
