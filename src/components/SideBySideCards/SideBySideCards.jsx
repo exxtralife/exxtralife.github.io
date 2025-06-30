98e9975ef13bcf7646ddb5c3da2d7ad7635b428e
psyje6 <psyje6@nottingham.ac.uk>
2025-06-30 15:45:58 +0100

    Photo cards appear stacked when aspect ratio becomes portrait

--- src/components/SideBySideCards/SideBySideCards.jsx
+++ src/components/SideBySideCards/SideBySideCards.jsx
@@ -2,31 +2,14 @@ import './SideBySideCards.css';
 
 const SideBySideCards = ({url1, url2}) => 
 {
-      var isPhone = window.screen.width < window.screen.height;
-
-      if(isPhone)
-        {
-          return (
-          <>
-            <div className='ticket-spread'>      
-              <img src={url1} width="100%"/>
-              <img src={url2}/>
-            </div>
-          </>
-          )
-        }
-
-      else
-        {
-          return (
-          <>
-            <div className='ticket-spread'>      
-              <img src={url1}/>
-              <img src={url2}/>
-            </div>
-          </>
-          )
-        }
-      
+    return (
+    <>
+      <div className='ticket-spread'>      
+        <img src={url1}/>
+        <img src={url2}/>
+      </div>
+    </>
+    )
 }
+     
 export default SideBySideCards
