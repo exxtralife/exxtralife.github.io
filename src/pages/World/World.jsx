058234d69d948b8ce8279007fcbc179887a3fe0b
edwxn-13 <73799683+edwxn-13@users.noreply.github.com>
2025-07-01 22:23:08 +0100

    easter eggs

--- src/pages/World/World.jsx
+++ src/pages/World/World.jsx
@@ -3,10 +3,15 @@ import { Link } from "react-router-dom";
 
 import image1Url from '../../assets/posters/anniversary/1.png'
 import image2Url from '../../assets/posters/anniversary/2.png'
+import never from '../../assets/posters/anniversary/never.png'
 
 import loop from "../../assets/posters/loop/ticket.png"
+import looppic from "../../assets/posters/loop/pic.png"
+
 import three from "../../assets/posters/three/three.png"
 import lite from "../../assets/posters/liteone/ticket.png"
+import litetwo from "../../assets/posters/litetwo/litetwo.png"
+
 import Navigation from '../../components/Navbar/Navbar';
 import SideBySideCards from '../../components/SideBySideCards/SideBySideCards';
 import SingleTicket from '../../components/SideBySideCards/SingleTicket';
@@ -19,8 +24,6 @@ function World() {
   document.body.style = 'background: white;';
 
   return (
-
-
     <div className="displayPane">
       <div className="world-page">
 
@@ -29,13 +32,24 @@ function World() {
             url2={image2Url}/>
         </Link>
 
-          <SingleTicket url1={loop}/>
-          
-          <SingleTicket url1={three}/>
+        <SingleTicket url1={never}/>
 
-          <a href = "https://edwxn-13.github.io/exxtralife/exxtralife.html">
+          <a href = "https://exxtralife.github.io/old-site/exxtramail/litepage.html">
+            <SingleTicket url1={loop}/>
+          </a>
+
+           <a href = "https://exxtralife.github.io/old-site/exxtramail/litepage.html">
+            <SingleTicket url1={litetwo}/>
+          </a>
+    
+          <a href = "https://exxtralife.github.io/old-site/Desktop/Desktop.html">
+            <SingleTicket url1={three}/>
+          </a>
+
+          <a href = "https://exxtralife.github.io/old-site/Desktop/product/exxtralife-three.html">
             <SingleTicket url1={lite}/>
           </a>
+
           <Navigation/>
       </div>
 
