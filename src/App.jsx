651f48108ef7ec87e3c2260f8e957aed8442d542
edwxn-13 <73799683+edwxn-13@users.noreply.github.com>
2025-07-02 19:54:46 +0100

    hashing

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
-import { BrowserRouter as Router, Route, Routes, useLocation } from 'react-router-dom';
+import { BrowserRouter as Router, HashRouter, Route, Routes, useLocation } from 'react-router-dom';
 
 import './App.css'
 import EscapeWillMakeMe from './pages/escape';
@@ -29,9 +29,9 @@ function AnimatedRoutes() {
 
 function App() {
   return (
-      <Router>
+      <HashRouter>
          <AnimatedRoutes/>
-      </Router>
+      </HashRouter>
   )
 }
 export default App
